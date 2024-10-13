fbfbc0010b100a8aacbece449054e712ba6a7711
JacobC117 <cvetanovskijacob@gmail.com>
2024-10-13 21:11:35 +1100

    sadasd

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react"
-
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:3002")
 
 function RadarChart() {
 
+  useEffect(() => {
+    socket.emit("send_message", {message: "I am a module!"})
+  }, [])
+
+  
+
   return (
     <div style={{width:'100%', height:'90vh'}}>
-      <p>Sending 1 notification!</p>
+      <p>Sending notification every 10 seconds!</p>
     </div>
   );
 }
