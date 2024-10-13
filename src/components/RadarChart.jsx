f6e95501155818fd144f9dfdfe50f2efa041eb8c
JacobC117 <cvetanovskijacob@gmail.com>
2024-10-13 21:00:07 +1100

    sdsds

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react"
-import io from 'socket.io-client'
-const socket = io.connect("http://localhost:3002")
+
 
 function RadarChart() {
-
-    socket.emit("send_message", {message: "I am a module!"})
-
 
   return (
     <div style={{width:'100%', height:'90vh'}}>
