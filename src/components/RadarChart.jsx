f89f95f4459726cf444bc1dbedfe20a77065df35
JacobC117 <cvetanovskijacob@gmail.com>
2024-10-13 20:14:47 +1100

    a

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -4,13 +4,15 @@ const socket = io.connect("http://localhost:3002")
 
 function RadarChart() {
 
+  setInterval(sendMessage, 10000);
+
   const sendMessage = () => {
     socket.emit("send_message", {message: "I am a module!"})
   };
 
   return (
-    <div style={{width:'100%', height:'90vh'}}>
-      <button onClick={sendMessage}>Test</button>
+    <div>
+      <p>Sending notification every 10 seconds!</p>
     </div>
   );
 }
