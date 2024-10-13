d366b37ea95a5bc75e8dc085449aabcf7d612f79
JacobC117 <cvetanovskijacob@gmail.com>
2024-10-13 18:52:13 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import StreamChart from './components/StreamChart';
 import BullChart from './components/BullChart';
 import SecureForm from './components/SecureForm';
 import RadarChart from './components/RadarChart';
-import TestModule from './components/testmodule';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -33,7 +32,6 @@ return (
     <Route path = "/bullmonitor" element={<BullChart/>}/>
     <Route path = "/incidentlog" element={<SecureForm/>}/>
     <Route path = "/Radar" element={<RadarChart/>}/>
-    <Route path = "/test" element={<TestModule/>}/>
     </Routes>
   </Router>
 );
